7f5cc8e54818f3e466c0a926d34ece15ee3cd83b
Luis Galeana <lsgaleana@gmail.com>
2023-12-27 22:43:53 -0600

    Add comments

--- utils/docker.py
+++ utils/docker.py
@@ -12,6 +12,7 @@ assert "DOCKER_NAME" in os.environ
 DOCKER_NAME = os.environ["DOCKER_NAME"]
 
 
+# Open "connection" with docker through the "-i" flag
 process = subprocess.Popen(
     ["docker", "exec", "-i", DOCKER_NAME, "bash"],
     stdin=subprocess.PIPE,
@@ -41,40 +42,46 @@ def execute(commands: List[str]) -> Tuple[List[str], List[str]]:
     assert process.stdout
     assert process.stderr
 
-    QUEUE = Queue[StdOut]()
+    queue = Queue[StdOut]()
 
     def _stream(pipe, q: Queue, output: Type[StdOut]) -> None:
+        # Wait for updates from docker
         while True:
             line = pipe.readline()
-            q.put(output(msg=line))
+            q.put(output(msg=line))  # Send message back to the main thread
             if not line or output.exit_signal in line:
                 break
 
-    stdout = threading.Thread(target=_stream, args=(process.stdout, QUEUE, StdOut))
-    stderr = threading.Thread(target=_stream, args=(process.stderr, QUEUE, StdErr))
+    # One thread for stdout, one thread for stderr
+    stdout = threading.Thread(target=_stream, args=(process.stdout, queue, StdOut))
+    stderr = threading.Thread(target=_stream, args=(process.stderr, queue, StdErr))
     stdout.start()
     stderr.start()
 
     outputs = []
     errors = []
     try:
+        # Iterate over commands
         for command in commands:
             process.stdin.write(f"{command}\n")
-            process.stdin.write(f"echo {COMMAND_EXECUTED}")
+            process.stdin.write(f"echo {COMMAND_EXECUTED}")  # Signal of executed
             process.stdin.flush()
 
-            output = QUEUE.get(timeout=TIMEOUT)
+            # Iterate over the command stdout or stderr
+            output = queue.get(timeout=TIMEOUT)
             while output.msg != COMMAND_EXECUTED:
                 if isinstance(output, StdOut):
                     outputs.append(output.msg)
                 else:
                     errors.append(output.msg)
                 print_system(output.msg, end="")
-                output = QUEUE.get(timeout=TIMEOUT)
+                output = queue.get(timeout=TIMEOUT)
 
+        # Signal to exit the threads
         process.stdin.write(f"echo {StdOut.exit_signal}\n")
         process.stdin.write(f"{StdErr.exit_signal}\n")
     except Empty:
+        # Timeout
         process.terminate()
         process = subprocess.Popen(
             ["docker", "exec", "-i", DOCKER_NAME, "bash"],
