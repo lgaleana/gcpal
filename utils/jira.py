89622acf62fd44bc993b0a9d828ec2a4b7b6d21b
Luis Galeana <lsgaleana@gmail.com>
2024-01-03 22:30:11 -0600

    Get all project issues

--- utils/jira.py
+++ utils/jira.py
@@ -2,13 +2,25 @@ import os
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-from typing import Any, Dict, Literal, Optional
+from pydantic import BaseModel
+from typing import Any, Dict, List, Literal, Optional
 
-URL = os.environ["JIRA_URL"]
+DOMAIN = "https://lsgaleana.atlassian.net/rest/api/3"
 AUTH = HTTPBasicAuth(os.environ["JIRA_EMAIL"], os.environ["JIRA_API_KEY"])
+HEADERS = {"Accept": "application/json", "Content-Type": "application/json"}
 
 
 IssueType = Literal["Epic", "Story", "Subtask"]
+
+
+class Issue(BaseModel):
+    type_: IssueType
+    key: str
+    title: str
+    description: str
+    status: str
+    children: List["Issue"]
+    parent_key: Optional[str] = None
 
 
 def create_issue(
@@ -38,9 +50,50 @@ def create_issue(
         issue_data["fields"]["parent"] = {"key": parent_key}
 
     response = requests.post(
-        URL,
+        f"{DOMAIN}/issue",
         data=json.dumps(issue_data),
-        headers={"Accept": "application/json", "Content-Type": "application/json"},
+        headers=HEADERS,
         auth=AUTH,
     )
     return response.json()
+
+
+def get_all_issues() -> List[Dict[str, Any]]:
+    jql_query = "project=SBX AND issuetype in ('Epic', 'Story', 'Subtask')"
+    response = requests.get(
+        f"{DOMAIN}/search?jql={jql_query}", headers=HEADERS, auth=AUTH
+    )
+    return response.json().get("issues", [])
+
+
+def get_grouped_issues() -> List[Dict[str, Any]]:
+    all_issues = get_all_issues()
+
+    epics = {}
+    stories = {}
+    subtasks = []
+    for raw_issue in all_issues:
+        issue = Issue(
+            type_=raw_issue["fields"]["issuetype"]["name"],
+            key=raw_issue["key"],
+            title=raw_issue["fields"]["summary"],
+            description=raw_issue["fields"]["description"]["content"][0]["content"][0][
+                "text"
+            ],
+            status=raw_issue["fields"]["status"]["name"],
+            children=[],
+            parent_key=raw_issue["fields"].get("parent", {}).get("key"),
+        )
+        if issue.type_ == "Epic":
+            epics[issue.key] = issue
+        elif issue.type_ == "Story":
+            stories[issue.key] = issue
+        elif issue.type_ == "Subtask":
+            subtasks.append(issue)
+
+    for story in stories.values():
+        epics[story.parent_key].children.append(story)
+    for subtask in subtasks:
+        stories[subtask.parent_key].children.append(subtask)
+
+    return list(epics.values())
