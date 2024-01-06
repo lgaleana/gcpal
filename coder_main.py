a3eee48be0150ef5012dc59c191d92880c694372
Luis Galeana <lsgaleana@gmail.com>
2024-01-05 18:17:56 -0600

    Add rollback mechanism

--- coder_main.py
+++ coder_main.py
@@ -8,7 +8,7 @@ load_dotenv()
 from ai_tasks import coder
 from utils import github
 from utils import jira
-from utils.files import create_files
+from utils.files import create_files, DIFFS_DIR
 from utils.docker import commands as docker, container
 from utils.io import user_input, print_system
 from utils.state import CommandStatus, Conversation
@@ -43,36 +43,55 @@ def run() -> None:
             if user_message != "y":
                 conversation.add_tool_response(
                     tool_id=ai_action.id,
-                    message=f"Didn't execute. Reason :: user feedback :: {user_message}",
+                    message=f"Didn't execute. Reason: user feedback :: {user_message}",
                 )
                 continue
 
+            # Create files locally first
             print_system("Copying files to container...")
-            root_path = create_files(tool.new_files, dir=session)
-            container.copy_files(
-                files=tool.new_files, root=root_path, container_path=f"/home/app"
-            )
-            root_path = create_files(tool.test_files, dir=session)
-            container.copy_files(
-                files=tool.test_files, root=root_path, container_path=f"/home/app"
-            )
-            docker.execute_one("git diff")
+            root_path = f"{DIFFS_DIR}/{session}"
+            create_files(tool.new_files + tool.test_files, root_path=root_path)
 
-            pip = docker.execute_one("python3 -m pip install -r requirements.txt")
-            if pip.status == CommandStatus.ERROR:
-                conversation.add_tool_response(
-                    tool_id=ai_action.id,
-                    message=f"Error running `{pip.command}` :: {pip.output_str()}",
+            try:
+                # 1. Copy them to docker container
+                container.copy_files(
+                    files=tool.new_files + tool.test_files,
+                    root=root_path,
+                    container_path=f"/home/app",
                 )
-                continue
-            pytest = docker.execute_one("python3 -m pytest")
-            if (
-                pytest.status == CommandStatus.ERROR
-                or "= ERRORS =" in pytest.output_str()
-            ):
-                conversation.add_tool_response(
-                    tool_id=ai_action.id,
-                    message=f"Error running `{pytest.command}` :: {pytest.output_str()}",
+                docker.execute_one("git diff")
+
+                # 2. Install new requirements
+                pip = docker.execute_one("python3 -m pip install -r requirements.txt")
+                if pip.status == CommandStatus.ERROR:
+                    conversation = _rollback(
+                        conversation,
+                        ai_action.id,
+                        f"running :: `{pip.command}`. Error :: {pip.output_str()}",
+                    )
+                    continue
+
+                # 3. Run tests
+                pytest = docker.execute_one("python3 -m pytest")
+                if (
+                    pytest.status == CommandStatus.ERROR
+                    or "= ERRORS =" in pytest.output_str()
+                ):
+                    conversation = _rollback(
+                        conversation,
+                        ai_action.id,
+                        f"running :: `{pytest.command}`. Error :: {pytest.output_str()}",
+                    )
+                    continue
+
+                # 4. Create commit
+
+                # 5. Push to github
+            except Exception as e:
+                conversation = _rollback(
+                    conversation,
+                    ai_action.id,
+                    str(e),
                 )
                 continue
 
@@ -90,3 +109,23 @@ def run() -> None:
 
 if __name__ == "__main__":
     run()
+
+
+def _rollback(conversation: Conversation, tool_id: str, agent_msg: str) -> Conversation:
+    print_system("Rolling back...")
+    docker.execute(
+        [
+            "cd /home/app",
+            "pwd",
+            "source venv/bin/activate",
+            "git restore .",  # roll back files
+            "git clean -fd",  # roll back files
+            "python3 -m pip uninstall -r requirements.txt -y",  # roll back packages
+            "python3 -m pip install -r requirements.txt",
+        ]
+    )
+    conversation.add_tool_response(
+        tool_id=tool_id,
+        message=f"Error :: {agent_msg}",
+    )
+    return conversation
--- utils/files.py
+++ utils/files.py
@@ -8,8 +8,7 @@ from ai_tasks.coder import File
 DIFFS_DIR = "gcpal-docker/diffs"
 
 
-def create_files(files: List[File], dir: str) -> str:
-    root_path = f"{DIFFS_DIR}/{dir}"
+def create_files(files: List[File], root_path: str) -> None:
     for file in files:
         full_path = f"{root_path}/{file.path}"
         intermediate_dirs, _ = os.path.split(full_path)
@@ -17,4 +16,3 @@ def create_files(files: List[File], dir: str) -> str:
             os.makedirs(intermediate_dirs, exist_ok=True)
         with open(full_path, "w") as f:
             f.write(file.content)
-    return root_path
